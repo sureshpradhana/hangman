183cc67be5ffb45c605b767115dc90c9f6a365bf
codebrakerk <64320966+codebrakerk@users.noreply.github.com>
2022-07-15 11:36:32 +0530

    lives_visual

--- hangman.py
+++ hangman.py
@@ -1,13 +1,12 @@
 import random
 from words import words
+from hangman_visual import lives_visual_dict
 
 word = random.choice(words)
 while '_' in word or '-' in word or ' ' in word:
     word = random.choice(words)
-if len(word) >6 and len(word)<10:
-   lives = 8
-else:
-    lives=6
+
+lives = 7
 word_letters = set(word)
 used_letters = set()
 
@@ -22,12 +21,16 @@ while len(word_letters) > 0 and lives > 0:
         print("you have used this letter")
         checker = [letter if letter in used_letters else '-' for letter in word]
         print("The words you have used are: ",' '.join(checker))
-        print("")
+        print(lives_visual_dict[lives])
+        print()
+        
+
     #if userinput in word
     elif userInput in word:
         used_letters.add(userInput)
         print(f"left lives {lives} and {userInput} is in word")
         checker = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
         if userInput in word_letters:
             word_letters.remove(userInput)
             print(' '.join(checker))
@@ -36,8 +39,9 @@ while len(word_letters) > 0 and lives > 0:
         used_letters.add(userInput)
         lives = lives-1
         print(f"left lives {lives} and {userInput} is not in word")
+        print(lives_visual_dict[lives])
 
 if lives == 0:
     print(f" you are dead , word is {word}")
 else:
-    print(f" you won ,word was {word}")
+    print(f" you won ,word is {word}")
